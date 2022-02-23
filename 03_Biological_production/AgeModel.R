4ea53f5e601304375b2775bacaa48e92d2722c60
Colin Millar <colin.millar@ices.dk>
2022-02-23 15:10:32 +0100

    fix recruiting age mistake

--- 03_Biological_production/AgeModel.R
+++ 03_Biological_production/AgeModel.R
@@ -6,11 +6,13 @@ age <- 1:Amax
 Ninit <- 1000
 Nvec <- numeric(Amax)
 M <- rep(0.2, Amax)
-Fmort <- c(0, 0, rep(0.3,Amax-2))
+Fmort <- c(0, 0, rep(0.3, Amax-2))
 
 Nvec[1] <- Ninit
-for(a in 1:(Amax-1))
+for (a in 1:(Amax-1)) {
   Nvec[a+1] <- Nvec[a] * exp(-M[a]-Fmort[a])
+}
+
 
 data.frame(age, Nvec, M, Fmort)
 
@@ -61,15 +63,21 @@ for(t in 1:(Tmax-1))
 
 round(N)
 
-SR <- data.frame(year, SSB=colSums(SSB), Rec=N[1,])
+rec_age <- 1
+SR <-
+  data.frame(
+    year = year[1:(Tmax - rec_age)],
+    SSB = colSums(SSB)[1:(Tmax - rec_age)],
+    Rec = N[1, (rec_age+1):Tmax]
+  )
 
 SSB.curve <- seq(0, 12000, by=500)
 Rec.curve <- (alpha*SSB.curve) / (beta+SSB.curve)
 
 par(mfrow=c(2,1), oma=c(0,0,0,3))
-plot(year, SR$SSB, ylim=c(0,12000), type="l", lwd=2, col="blue", xlab="Year",
+plot(SR$year, SR$SSB, ylim=c(0,12000), type="l", lwd=2, col="blue", xlab="Year",
      ylab="SSB (t)")
-lines(year, SR$Rec*10, lwd=2, col="red")
+lines(SR$year, SR$Rec * 10, lwd = 2, col = "red")
 axis(4, at=seq(0,12000,by=2000), labels=seq(0,1200,by=200))
 mtext("Recruitment at age 1 (1000s)", side=4, line=3)
 
@@ -97,7 +105,7 @@ for(t in 1:(Tmax-1))
   SSB[,t+1] <- N[,t+1] * mat * w
 }
 
-round(N)
+round(N, 3)
 
 SR <- data.frame(year, SSB=colSums(SSB), Rec=N[1,])
 
